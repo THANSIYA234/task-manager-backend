24abecc2638c065ff4b653f4495dd6fcf55865ae
thansiya <thansiya234@gmail.com>
2026-02-20 20:26:57 +0400

    Fix CORS for frontend ports

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
   });
   app.setGlobalPrefix('api');
 
