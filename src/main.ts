eb60a85f727c47d693024179a9794c7ff4f83da2
thansiya <thansiya234@gmail.com>
2026-02-20 20:32:41 +0400

    Fix CORS properly

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'https://your-frontend-name.onrender.com', // add later
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
