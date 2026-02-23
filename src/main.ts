78b53eb0cf238eba23b04a2c893c0ffbe0c6a92d
thansiya <thansiya234@gmail.com>
2026-02-23 21:11:11 +0400

    add url

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://task-manager-frontend-production-9499.up.railway.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
